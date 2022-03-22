141459e9931dd4a8eecf66ec60ab7ae869f0e073
natacha <nat_bartu@hotmail.com>
2022-03-22 15:10:19 +1000

    add deploy link to readme

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 This app was intentionally for learning how to get data from API and display videos using react.
 I used Semantic-ui for a bit of style.
 The videos used on this app it is from [youtube](https://youtube.com)
+
+# Deployment
+[videos](https://videos-hooks-lac-alpha.vercel.app)
--- src/components/VideoDetails/VideoDetail.js
+++ src/components/VideoDetails/VideoDetail.js
@@ -5,18 +5,12 @@ const VideoDetail = ({ video }) => {
     return <div>Loading...</div>;
   }
 
-  const videoSrc = `http://www.youtube.com/embed/${video.id.videoId}`;
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
 
   return (
     <div>
       <div className="ui embed">
-        <iframe
-          title="video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-          src={videoSrc}
-        />
+        <iframe title="video player" src={videoSrc} />
       </div>
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
